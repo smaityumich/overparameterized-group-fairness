84bf78f9c511d7da69396e21d8907e83e6465044
Subha Maity <smaity@gl-login2.arc-ts.umich.edu>
2020-12-14 01:55:44 -0500

    Done

--- sim.py
+++ sim.py
@@ -34,7 +34,7 @@ n, p = 3000, 0.9
 n1, n2 = int(n * p), int(n * (1-p))
 x1, x2 = np.random.normal(size = (n1, 10)), np.random.normal(size = (n2, 10))
 #beta, delta = 10* np.array([1/np.sqrt(10)] * 10).reshape((-1,1)), 10*np.array([-2/np.sqrt(10)] + [0]*9).reshape((-1,1))
-beta_norm = 1
+beta_norm = 10
 beta, delta = beta_norm * np.array([1] * 5 + [0] * 5).reshape((-1,1)), np.sqrt(1)*np.array([0] * 5 + [1]*5).reshape((-1,1))
 y1, y2 = x1 @ (beta + delta) + 0.1 * np.random.normal(size=(n1, 1)), x2 @ (beta - delta) + 0.1 * np.random.normal(size = (n2, 1))
 sample_weights = np.array([(1-p)] * n1 + [p] * n2)
@@ -57,7 +57,7 @@ if not os.path.exists('temp/'):
     os.mkdir('temp/')
 
 
-with open(f'temp/mse_{iteration}.txt', 'w') as f:
+with open(f'temp/mse_{iteration}_{beta_norm}.txt', 'w') as f:
     for nodes, epochs in itertools.product(nodes_list, epochs_list):
         train_mse, train_mse_bal, majority_mse, minority_mse = mse_overparameter(train_data, test_majority,\
              test_minority, nodes=int(nodes), optimizer='Adam', epochs=epochs)
