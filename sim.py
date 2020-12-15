c1c20a3a40a87cf2aa692fc1c393d282662b3ff6
Subha Maity <smaity@gl-login2.arc-ts.umich.edu>
2020-12-14 22:58:37 -0500

    Done

--- sim.py
+++ sim.py
@@ -48,29 +48,29 @@ for beta_norm in beta_norms:
     x1, x2 = np.random.normal(size = (n1, 10)), np.random.normal(size = (n2, 10))
     #beta, delta = 10* np.array([1/np.sqrt(10)] * 10).reshape((-1,1)), 10*np.array([-2/np.sqrt(10)] + [0]*9).reshape((-1,1))
     beta, delta = beta_norm * np.array([1] * 5 + [0] * 5).reshape((-1,1)), np.sqrt(1)*np.array([0] * 5 + [1]*5).reshape((-1,1))
-    y1, y2 = x1 @ (beta +0* delta) + 0.1 * np.random.normal(size=(n1, 1)), x2 @ (beta+ delta) + 0.1 * np.random.normal(size = (n2, 1))
+    y1, y2 = x1 @ (beta +1* delta) + 0.1 * np.random.normal(size=(n1, 1)), x2 @ (beta - delta) + 0.1 * np.random.normal(size = (n2, 1))
     sample_weights = np.array([(1-p)] * n1 + [p] * n2)
     train_data = np.vstack((x1, x2)), np.vstack((y1, y2)), sample_weights
 
 
     x_test = np.random.normal(size = (1000, 10))
-    y_test = x_test @ (beta + delta) + 0.1 * np.random.normal(size = (1000, 1))
+    y_test = x_test @ (beta - delta) + 0.1 * np.random.normal(size = (1000, 1))
     test_minority = x_test, y_test
 
     x_test = np.random.normal(size = (1000, 10))
-    y_test = x_test @ (beta + 0*delta) + 0.1 * np.random.normal(size = (1000, 1))
+    y_test = x_test @ (beta + 1*delta) + 0.1 * np.random.normal(size = (1000, 1))
     test_majority = x_test, y_test
 
 
 
 
-    with open(f'temp/mse_{iteration}_{beta_norm}.txt', 'w') as f:
+    with open(f'temp/mse_{iteration}_{beta_norm}_s.txt', 'w') as f:
         for nodes, epochs in itertools.product(nodes_list, epochs_list):
             train_mse, train_mse_bal, majority_mse, minority_mse = mse_overparameter(train_data, test_majority,\
                 test_minority, nodes=int(nodes), optimizer='Adam', epochs=epochs)
             output = {'nodes': nodes, 'beta-norm': beta_norm, 'epochs': epochs, 'train-mse':train_mse,\
                 'train-mse-bal': train_mse_bal, 'majority-mse': majority_mse,\
-                    'minority-mse': minority_mse, 'trainable': 'last-layer', 'setup': 'different-core-over-envs'}
+                    'minority-mse': minority_mse, 'trainable': 'last-layer', 'setup': 'same-core-over-envs'}
             f.writelines(str(output)+"\n")
     
 
