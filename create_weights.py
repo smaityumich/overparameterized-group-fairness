bcb524d72a1fbf85b1eadc8b3af6569c953b776a
Subha Maity <smaity@smaity-mbp.local>
2021-01-20 16:17:24 +0530

    Done

---
+++ create_weights.py
@@ -0,0 +1,15 @@
+import numpy as np
+np.random.seed(1)
+d = 10
+gammas = np.logspace(0, 4, num = 10)
+nodes_list = np.rint(10 * gammas).astype('int')
+
+
+def sample_from_sphere(n, d):
+    x = np.random.normal(size = (n, d))
+    x = (x.T/np.linalg.norm(x, axis = 1)).T
+    return x
+
+for nodes in nodes_list:
+    w = sample_from_sphere(d, nodes)
+    np.save(f'weights/w_{d}_{nodes}.npy', w)
