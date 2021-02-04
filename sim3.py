5919091b1d7310930d1538138b05a66d6031d5f0
Subha Maity <smaity@smaity-mbp.local>
2021-02-04 16:02:13 -0500

    Done

--- sim3.py
+++ sim3.py
@@ -60,8 +60,8 @@ gammas = np.logspace(0, 4 , num = 10)
 nodes_list = np.rint(10 * gammas).astype('int')
 
 
-if not os.path.exists('temp_beta_delta/'):
-    os.mkdir('temp_beta_delta/')
+if not os.path.exists('ERM_beta_delta/'):
+    os.mkdir('ERM_beta_delta/')
 
 
 for SNR in SNRs:
@@ -106,7 +106,7 @@ for SNR in SNRs:
 
     x1, x2 = np.random.normal(size = (n1, 10)), np.random.normal(size = (n2, 10))
     beta, delta = np.array([1] * 5 + [0] * 5).reshape((-1,1)), np.sqrt(1)*np.array([0] * 5 + [1]*5).reshape((-1,1))
-    beta, delta =  5 * beta / np.linalg.norm(beta), (1/SNR) * delta / np.linalg.norm(delta)
+    beta, delta =   SNR * beta / np.linalg.norm(beta),  delta / np.linalg.norm(delta)
     y1, y2 = x1 @ (beta + delta) + sigma * np.random.normal(size=(n1, 1)), x2 @ (beta) + sigma * np.random.normal(size = (n2, 1))
     sample_weights = np.array([(1-p)] * n1 + [p] * n2)
     train_data = np.vstack((x1, x2)), np.vstack((y1, y2)), sample_weights
@@ -123,7 +123,7 @@ for SNR in SNRs:
 
 
 
-    with open(f'temp_beta_delta/mse_{iteration}_{SNR}_diff.txt', 'w') as f:
+    with open(f'ERM_beta_delta/mse_{iteration}_{SNR}_diff.txt', 'w') as f:
         for nodes in nodes_list:
             train_mse, train_mse_bal, majority_mse, minority_mse = mse_overparameter(train_data, test_majority,\
                 test_minority, nodes=int(nodes), weighted=False)
@@ -132,11 +132,11 @@ for SNR in SNRs:
                     'minority-mse': minority_mse, 'trainable': 'last-layer', 'setup': 'diff-core'}
             f.writelines(str(output)+"\n")
     
-            train_mse, train_mse_bal, majority_mse, minority_mse = mse_overparameter(train_data, test_majority,\
-                test_minority, nodes=int(nodes), weighted=True)
-            output = {'optimization': 'weighted-ERM', 'nodes': nodes, 'SNR': SNR, 'train-mse':train_mse,\
-                'train-mse-bal': train_mse_bal, 'majority-mse': majority_mse,\
-                    'minority-mse': minority_mse, 'trainable': 'last-layer', 'setup': 'diff-core'}
-            f.writelines(str(output)+"\n")
+            # train_mse, train_mse_bal, majority_mse, minority_mse = mse_overparameter(train_data, test_majority,\
+            #     test_minority, nodes=int(nodes), weighted=True)
+            # output = {'optimization': 'weighted-ERM', 'nodes': nodes, 'SNR': SNR, 'train-mse':train_mse,\
+            #     'train-mse-bal': train_mse_bal, 'majority-mse': majority_mse,\
+            #         'minority-mse': minority_mse, 'trainable': 'last-layer', 'setup': 'diff-core'}
+            # f.writelines(str(output)+"\n")
     
 
